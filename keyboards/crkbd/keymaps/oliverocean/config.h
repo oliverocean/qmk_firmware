98c2eb8546a7ffab60bf27238b55e6b6aa05cce4
Oliver Ocean <github@oliverocean.co>
2024-01-16 11:27:59 -0600

    minor edit

--- keyboards/crkbd/keymaps/oliverocean/config.h
+++ keyboards/crkbd/keymaps/oliverocean/config.h
@@ -1,12 +1,5 @@
 #pragma once
 
-// #define MASTER_LEFT
-#define SPLIT_USB_DETECT
-
-#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
-// #define QUICK_TAP_TERM 0
-
-#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
+#define MASTER_LEFT
 
 // eof
